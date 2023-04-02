28071202f8463131b8f6a3917c3afa8f2a74b0ba
hardness1020 <hardness1020@gmail.com>
2023-04-02 23:33:37 +0800

    feat: initialization

---
+++ HW2/hw2_r10631026.sql
@@ -0,0 +1,274 @@
+/* create and use database */
+CREATE DATABASE HealthyDB;
+USE HealthyDB;
+
+/* info */
+CREATE TABLE self (
+    student_id VARCHAR(10) NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    department VARCHAR(10) NOT NULL,
+    year VARCHAR(10) NOT NULL,
+    PRIMARY KEY (student_id)
+);
+
+
+/* create table */
+CREATE TABLE User (
+    user_id INT AUTO_INCREMENT,
+    username VARCHAR(40) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    PRIMARY KEY (user_id),
+    CHECK (username != password)
+);
+
+CREATE TABLE Admin (
+    user_id INT UNIQUE NOT NULL,
+    database_account VARCHAR(40),
+    cloud_account VARCHAR(40),
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_admin_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Normal (
+    user_id INT UNIQUE NOT NULL,
+    self_inrtoduction VARCHAR(40),
+    address ENUM('Taipei', 'Taichung', 'Kaohsiung', 'Other', 'Unknown') DEFAULT 'Unknown',
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_normal_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Promoter (
+    user_id INT UNIQUE NOT NULL,
+    total_profit FLOAT DEFAULT 0,
+    excepted_profit FLOAT DEFAULT 0,
+    created_by_admin_id INT NOT NULL,
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_promoter_user_id FOREIGN KEY (user_id) REFERENCES User(user_id),
+    CONSTRAINT fk_promoter_created_by_admin_id FOREIGN KEY (created_by_admin_id) REFERENCES Admin(user_id) 
+);
+
+CREATE TABLE Customer (
+    user_id INT UNIQUE NOT NULL,
+    consumption INT DEFAULT 0,
+    is_active ENUM('Y', 'N') DEFAULT 'N',
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_customer_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Author (
+    user_id INT UNIQUE NOT NULL,
+    total_liked_number INT DEFAULT 0,
+    total_saved_number INT DEFAULT 0,
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_author_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Coupon (
+    promoter_id INT NOT NULL,
+    code VARCHAR(40) NOT NULL,
+    discount FLOAT NOT NULL,
+    expire_date DATE NOT NULL,
+    CONSTRAINT fk_coupon_promoter_id FOREIGN KEY (promoter_id) REFERENCES Promoter(user_id),
+    PRIMARY KEY (promoter_id, code),
+    CHECK (discount > 0 AND discount < 1)
+);
+
+CREATE TABLE Health (
+    health_id INT NOT NULL,
+    precaution VARCHAR(40) NOT NULL,
+    suggestion VARCHAR(40) NOT NULL,
+    PRIMARY KEY (health_id)
+);
+
+CREATE TABLE Disease (
+    disease_id INT NOT NULL,
+    severity ENUM('low', 'medium', 'high') NOT NULL,
+    action_plan VARCHAR(40) NOT NULL,
+    PRIMARY KEY (disease_id)
+);
+
+CREATE TABLE Report (
+    report_id INT NOT NULL,
+    date DATE NOT NULL,
+    content VARCHAR(40) NOT NULL,
+    author_id INT NOT NULL,
+    previous_version_id INT UNIQUE,
+    inherited_health_id INT UNIQUE,
+    inherited_disease_id INT UNIQUE,
+    PRIMARY KEY (report_id),
+    CONSTRAINT fk_report_author_id FOREIGN KEY (author_id) REFERENCES Author(user_id),
+    CONSTRAINT fk_report_previous_version_id FOREIGN KEY (previous_version_id) REFERENCES Report(report_id),
+    CONSTRAINT fk_report_inherited_health_id FOREIGN KEY (inherited_health_id) REFERENCES Health(health_id),
+    CONSTRAINT fk_report_inherited_disease_id FOREIGN KEY (inherited_disease_id) REFERENCES Disease(disease_id),
+    CHECK (inherited_health_id IS NOT NULL OR inherited_disease_id IS NOT NULL)
+);
+
+CREATE TABLE Category (
+    category_id INT NOT NULL,
+    name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE Article (
+    article_id INT NOT NULL,
+    date DATE NOT NULL,
+    content VARCHAR(40) NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY (article_id),
+    CONSTRAINT fk_article_author_id FOREIGN KEY (author_id) REFERENCES Author(user_id)
+);
+
+CREATE TABLE ArticleCategory (
+    article_id INT NOT NULL,
+    category_id INT NOT NULL,
+    CONSTRAINT fk_article_id FOREIGN KEY (article_id) REFERENCES Article(article_id),
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Category(category_id),
+    PRIMARY KEY (article_id, category_id)
+);
+
+CREATE TABLE CustomerBuyReport (
+    customer_id INT NOT NULL,
+    article_id INT NOT NULL,
+    coupon_promoter_id INT,
+    coupon_code VARCHAR(40),
+    CONSTRAINT fk_buy_customer_id FOREIGN KEY (customer_id) REFERENCES Customer(user_id),
+    CONSTRAINT fk_buy_article_id FOREIGN KEY (article_id) REFERENCES Article(article_id),
+    CONSTRAINT fk_buy_coupon FOREIGN KEY (coupon_promoter_id, coupon_code) REFERENCES Coupon(promoter_id, code),
+    PRIMARY KEY (customer_id, article_id)
+);
+
+CREATE TABLE UserLikeArticle (
+    user_id INT NOT NULL,
+    article_id INT NOT NULL,
+    CONSTRAINT fk_like_user_id FOREIGN KEY (user_id) REFERENCES User(user_id),
+    CONSTRAINT fk_like_article_id FOREIGN KEY (article_id) REFERENCES Article(article_id),
+    PRIMARY KEY (user_id, article_id)
+);
+
+CREATE TABLE UserSaveArticle (
+    user_id INT NOT NULL,
+    article_id INT NOT NULL,
+    CONSTRAINT fk_save_user_id FOREIGN KEY (user_id) REFERENCES User(user_id),
+    CONSTRAINT fk_save_article_id FOREIGN KEY (article_id) REFERENCES Article(article_id),
+    PRIMARY KEY (user_id, article_id)
+);
+
+/* insert */
+INSERT INTO `self` VALUES ('r10631026', '張名翔', '生機系', '碩二');
+
+INSERT INTO `Health` (health_id, precaution, suggestion) VALUES (1, '避免過度練習', '多吃蔬菜水果');
+INSERT INTO `Health` (health_id, precaution, suggestion) VALUES (2, '避免勞累', '多睡眠');
+INSERT INTO `Health` (health_id, precaution, suggestion) VALUES (3, '不要抽菸', '多運動');
+INSERT INTO `Disease` (disease_id, severity, action_plan) VALUES (1, 'low', '多休息');
+INSERT INTO `Disease` (disease_id, severity, action_plan) VALUES (2, 'medium', '多吃藥');
+INSERT INTO `Disease` (disease_id, severity, action_plan) VALUES (3, 'high', '定期複檢');
+
+INSERT INTO `User` (username, password) VALUES ('smith', 'password123');
+SET @smith_id = LAST_INSERT_ID();
+INSERT INTO `Admin` (user_id) VALUES (@smith_id);
+INSERT INTO `Normal` (user_id) VALUES (@smith_id);
+INSERT INTO `Promoter` (user_id, created_by_admin_id) VALUES (@smith_id, @smith_id);
+INSERT INTO `Coupon` (promoter_id, code, discount, expire_date) VALUES (@smith_id, '123456', 0.5, '2020-12-31');
+INSERT INTO `Author` (user_id) VALUES (@smith_id);
+INSERT INTO `Report` (report_id, date, content, author_id, inherited_health_id) 
+              VALUES (1, '2020-01-01', 'test1', @smith_id, 1);
+INSERT INTO `Category` (category_id, name) VALUES (1, 'test1');
+INSERT INTO `Article` (article_id, date, content, author_id) 
+               VALUES (1, '2020-01-01', 'test1', @smith_id);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (1, 1);
+
+
+INSERT INTO `User` (username, password) VALUES ('doe', 'secret456');
+SET @doe_id = LAST_INSERT_ID();
+INSERT INTO `Admin` (user_id) VALUES (@doe_id);
+INSERT INTO `UserLikeArticle` (user_id, article_id) VALUES (@doe_id, 1);
+INSERT INTO `UserSaveArticle` (user_id, article_id) VALUES (@doe_id, 1);
+INSERT INTO `Customer` (user_id, consumption) VALUES (@doe_id, 5000);
+INSERT INTO `CustomerBuyReport` (customer_id, article_id) VALUES (@doe_id, 1);
+
+INSERT INTO `User` (username, password) VALUES ('ross', 'happytrees789');
+SET @ross_id = LAST_INSERT_ID();
+INSERT INTO `Admin` (user_id) VALUES (@ross_id);
+INSERT INTO `Author` (user_id) VALUES (@ross_id);
+INSERT INTO `Report` (report_id, date, content, author_id, inherited_disease_id) 
+              VALUES (2, '2021-01-01', 'test2', @ross_id, 2);
+INSERT INTO `Category` (category_id, name) VALUES (2, 'test2');
+INSERT INTO `Article` (article_id, date, content, author_id) 
+               VALUES (2, '2021-01-01', 'test2', @ross_id);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (2, 1);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (2, 2);
+INSERT INTO `UserLikeArticle` (user_id, article_id) VALUES (@ross_id, 1);
+INSERT INTO `UserLikeArticle` (user_id, article_id) VALUES (@ross_id, 2);
+INSERT INTO `UserSaveArticle` (user_id, article_id) VALUES (@ross_id, 1);
+
+INSERT INTO `User` (username, password) VALUES ('john', 'pass9836rd123');
+SET @john_id = LAST_INSERT_ID();
+INSERT INTO `Normal` (user_id) VALUES (@john_id);
+INSERT INTO `Promoter` (user_id, created_by_admin_id) VALUES (@john_id, @smith_id);
+INSERT INTO `Coupon` (promoter_id, code, discount, expire_date) VALUES (@john_id, '12345678', 0.5, '2025-12-31');
+INSERT INTO `Customer` (user_id, consumption) VALUES (@john_id, 1000);
+INSERT INTO `CustomerBuyReport` (customer_id, article_id) VALUES (@john_id, 2);
+
+INSERT INTO `User` (username, password) VALUES ('bob', 'pass564rd123');
+SET @bob_id = LAST_INSERT_ID();
+INSERT INTO `Normal` (user_id) VALUES (@bob_id);
+INSERT INTO `Author` (user_id) VALUES (@bob_id);
+INSERT INTO `Report` (report_id, date, content, author_id, inherited_disease_id) 
+              VALUES (3, '2022-01-01', 'test3', @bob_id, 3);
+INSERT INTO `Category` (category_id, name) VALUES (3, 'test3');
+INSERT INTO `Article` (article_id, date, content, author_id) 
+               VALUES (3, '2022-01-01', 'test3', @bob_id);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (3, 1);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (3, 2);
+INSERT INTO `ArticleCategory` (article_id, category_id) VALUES (3, 3);
+
+INSERT INTO `User` (username, password) VALUES ('jane', 'paefeword123');
+SET @jane_id = LAST_INSERT_ID();
+INSERT INTO `Normal` (user_id) VALUES (@jane_id);
+INSERT INTO `Promoter` (user_id, created_by_admin_id) VALUES (@jane_id, @smith_id);
+INSERT INTO `Coupon` (promoter_id, code, discount, expire_date) VALUES (@jane_id, '123456789', 0.5, '2025-12-31');
+INSERT INTO `Customer` (user_id, consumption) VALUES (@jane_id, 1000);
+INSERT INTO `CustomerBuyReport` (customer_id, article_id) VALUES (@jane_id, 2);
+INSERT INTO `CustomerBuyReport` (customer_id, article_id) VALUES (@jane_id, 3);
+INSERT INTO `UserLikeArticle` (user_id, article_id) VALUES (@jane_id, 2);
+INSERT INTO `UserSaveArticle` (user_id, article_id) VALUES (@jane_id, 2);
+
+
+
+
+/* create two views (Each view should be based on two tables.)*/
+CREATE VIEW customer_with_high_severity_report AS
+SELECT Customer.user_id, User.username, Report.report_id FROM User, Customer, CustomerBuyReport, Report, Disease
+WHERE  Disease.severity = 'high' AND
+       Customer.user_id = CustomerBuyReport.customer_id AND
+       CustomerBuyReport.article_id = Report.report_id AND
+       Report.inherited_disease_id = Disease.disease_id AND
+       User.user_id = Customer.user_id;
+
+CREATE VIEW promoter_with_non_expired_coupon AS
+Select Promoter.user_id, User.username, Coupon.code, Coupon.discount, Coupon.expire_date FROM User, Promoter, Coupon
+WHERE  Coupon.expire_date > NOW() AND
+       User.user_id = Promoter.user_id AND
+       Promoter.user_id = Coupon.promoter_id;
+
+
+/* select from all tables and views */
+SELECT * FROM self;
+SELECT * FROM User;
+SELECT * FROM Admin;
+SELECT * FROM Normal;
+SELECT * FROM Promoter;
+SELECT * FROM Coupon;
+SELECT * FROM Author;
+SELECT * FROM Report;
+SELECT * FROM Category;
+SELECT * FROM Article;
+SELECT * FROM ArticleCategory;
+SELECT * FROM Customer;
+SELECT * FROM CustomerBuyReport;
+SELECT * FROM UserLikeArticle;
+SELECT * FROM UserSaveArticle;
+
+
+/* drop database */
+-- DROP DATABASE HealthyDB;
